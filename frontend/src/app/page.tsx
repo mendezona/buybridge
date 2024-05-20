2023c0edeb97d1aa95ac5f7b84a09d1c802aae30
Joseph Mendezona <joseph.mendezona@pwc.com>
2024-05-20 16:13:28 +0200

    add item database connection

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 
 // TODO: Remove this comment
-// import { CreatePost } from "~/app/_components/create-post";
+import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -37,31 +35,20 @@ export default async function Home() {
             </div>
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        {/* <CrudShowcase /> */}
+        <CrudShowcase />
       </div>
     </main>
   );
 }
 
-// TODO: Remove this comment
-// async function CrudShowcase() {
-//   const latestPost = await api.post.getLatest();
+async function CrudShowcase() {
+  const { data } = api.items.getAll.useQuery();
 
-//   return (
-//     <div className="w-full max-w-xs">
-//       {latestPost ? (
-//         <p className="truncate">Your most recent post: {latestPost.name}</p>
-//       ) : (
-//         <p>You have no posts yet.</p>
-//       )}
+  return (
+    <div className="w-full max-w-xs">
+      <div>{data?.map((item) => <div key={item.id}>{item.name}</div>)}</div>
 
-//       <CreatePost />
-//     </div>
-//   );
-// }
+      {/* <CreatePost /> */}
+    </div>
+  );
+}
