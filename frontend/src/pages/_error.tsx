3e2ac0c38828880498af160cd647116040c9c367
Joseph Mendezona <joseph_mendezona@hotmail.com>
2024-06-03 18:27:24 +0200

    convert jsx file to tsx file sentry

--- frontend/src/pages/_error.tsx
+++ frontend/src/pages/_error.tsx
@@ -1,17 +1,21 @@
 import * as Sentry from "@sentry/nextjs";
+import { type NextPageContext } from "next";
 import Error from "next/error";
+import React from "react";
 
-const CustomErrorComponent = (props) => {
+const CustomErrorComponent = (props: { statusCode: number }) => {
   return <Error statusCode={props.statusCode} />;
 };
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
+CustomErrorComponent.getInitialProps = async (props: {
+  contextData: NextPageContext;
+}) => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
-  await Sentry.captureUnderscoreErrorException(contextData);
+  await Sentry.captureUnderscoreErrorException(props.contextData);
 
   // This will contain the status code of the response
-  return Error.getInitialProps(contextData);
+  return Error.getInitialProps(props.contextData);
 };
 
 export default CustomErrorComponent;
